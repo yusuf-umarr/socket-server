ea6d734aea56e01f2e71c9b74874edd8bb48e89b
mac <mac@macs-MacBook-Pro.local>
2023-11-25 16:23:43 +0100

    updated: get request

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 var http = require("http");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5555;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 
 //middlewre
+
+app.get("/", (req, res)=>{
+  res.write(`Socket server started!!! on paort : ${port} `);
+  res.end();
+})
 app.use(express.json());
 var clients = {};
 
